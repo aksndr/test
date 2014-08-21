5c2b9c7560b1e2c25996966333935adb35605e25
aksndr <aksndr@gmail.com>
2014-08-21 20:38:17 +0400

    Added Counter model.

---
+++ src/main/java/ru/aksndr/model/counter.java
@@ -0,0 +1,36 @@
+package ru.aksndr.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by aksndr on 19.08.14.
+ */
+@Entity
+@Table(name = "counters")
+public class counter {
+    @Id
+    @Column(name = "sn", nullable = false, unique = true)
+    private String sn;
+
+    @Column(name = "descr", nullable = false)
+    private String descr;
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+}
